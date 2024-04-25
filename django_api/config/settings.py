af420c57092a112ea7f9b810a4b570dc0544c249
Dmitry Morozov <adm713934>
2024-04-25 23:43:05 +0300

    исполняемый файл uwsgi не найден внутри контейнера при его запуске.

--- django_api/config/settings.py
+++ django_api/config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 include(
     'components/apps.py',
-    'components/auth_password_validators',
+    'components/auth_password_validators.py',
     'components/database.py',
     'components/middlewares.py',
     'components/templates.py',
